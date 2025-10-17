3a6305b2f3f344f7a06fc8e8545939f14a29c415
chostmadsen <chostmadsen@gmail.com>
2025-10-17 02:38:24 -0700

    main setup and minor style changes

--- py_cnn/main.py
+++ py_cnn/main.py
@@ -1,36 +1,13 @@
-import torch
-import torch.nn as nn
-import torch.optim as optim
-from torchvision import datasets, transforms
 from network import CNN
-from helpers import write_tensor, write_label
-
-epochs: int = 1_000
-CNN = CNN()
-
-# 1. Load MNIST data
-transform = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize((0.1307,), (0.3081,))  # mean and std of MNIST
-])
-
-train_loader = torch.utils.data.DataLoader(
-    datasets.MNIST(root="./data", train=True, download=True, transform=transform),
-    batch_size=64,
-    shuffle=True
-)
-
-test_loader = torch.utils.data.DataLoader(
-    datasets.MNIST(root="./data", train=False, transform=transform),
-    batch_size=1000,
-    shuffle=False
-)
-
-# import mnist here??? fuck
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(CNN.parameters(), lr=0.01)
 
 
-for epoch in range(1, epochs):
-    # loop & shit
-    ...
+def main() -> None:
+    r"""
+    Main function.
+    """
+    epochs: int = 1_000
+    model: CNN = CNN()
+
+
+if __name__ == "__main__":
+    main()
